9cb7eb62d847995190d5215871294d4e51de900a
Joe Withers <joewithers96@gmail.com>
2017-11-23 22:32:53 +0000

    removed CMake files

--- CMakeFiles/Sponza_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "Sponza_automoc.cpp"
-  "CMakeFiles/Sponza_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Sponza_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
